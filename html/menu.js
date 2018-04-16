c5b61e1baeaad9018139ff51e930b9de76d48b95
Alex <acsouthwick@gmail.com>
2018-04-16 09:40:38 -0700

    changed text

--- html/menu.js
+++ html/menu.js
@@ -1,40 +1,40 @@
 var menuoptions =
 [
-  ["Introduction", "index.html", "---"],
+  ["Our Website", "index.html", "---"],
   ["Introduction", "index.html"],
   ["About Us", "signals.html"],
-  ["Discrete Signals", "discrete_signals.html"],
-  ["Sampling & Aliasing", "aliasing.html"],
-  ["Sound Waves", "sound.html"],
-  ["Timbre", "sound2.html"],
+  // ["Discrete Signals", "discrete_signals.html"],
+  // ["Sampling & Aliasing", "aliasing.html"],
+  // ["Sound Waves", "sound.html"],
+  // ["Timbre", "sound2.html"],
   ["Legislation", "part2.html", "---"],
   ["Federal", "sincos.html"],
   ["California", "trig_review.html"],
-  ["The Sampling Theorem", "sampling.html"],
-  ["Nyquist Frequency", "sampling2.html"],
-  ["Wagon Wheel Effect", "sampling4.html"],
-  ["Sine Wave Aliasing", "sampling3.html"],
+  // ["The Sampling Theorem", "sampling.html"],
+  // ["Nyquist Frequency", "sampling2.html"],
+  // ["Wagon Wheel Effect", "sampling4.html"],
+  // ["Sine Wave Aliasing", "sampling3.html"],
   ["Activism", "part3.html", "---"],
-  ["Representations & Transforms", "coordinates.html"],
-  ["The Fourier Transform", "dft_introduction.html"],
-  ["Notation", "notation.html"],
-  ["Complex Numbers", "complex.html"],
-  ["Euler’s Formula", "euler.html"],
-  ["Inside the DFT", "part4.html", "---"],
-  ["The Dot Product", "dotproduct.html"],
-  ["Correlation", "dotproduct2.html"],
-  ["Correlation With Sine", "dotproduct3.html"],
-  ["Correlation With Sine and Cosine", "dotproduct4.html"],
-  ["Discrete Fourier Transform Example", "dft_walkthrough.html"],
-  ["Digging Deeper", "dft_deeper.html"],
-  ["USAGE and INTERPRETATION", "part5.html", "---"],
-  ["Frequency Resolution", "dft_frequency.html"],
-  ["Zero-Padding", "zeropadding.html"],
-  ["Leakage", "dft_leakage.html"],
-  ["Sine Wave Properties", "sine_wave_properties.html"],
-  //["Convolution", "convolution.html"],
-  ["Using FFT Libraries", "fft.html"],
-  ["Conclusion", "conclusion.html"]
+  ["Direct Action", "coordinates.html"],
+  ["Contacts", "dft_introduction.html"],
+  // ["Notation", "notation.html"],
+  // ["Complex Numbers", "complex.html"],
+  // ["Euler’s Formula", "euler.html"],
+  ["Vote", "part4.html", "---"],
+  // ["The Dot Product", "dotproduct.html"],
+  // ["Correlation", "dotproduct2.html"],
+  // ["Correlation With Sine", "dotproduct3.html"],
+  // ["Correlation With Sine and Cosine", "dotproduct4.html"],
+  // ["Discrete Fourier Transform Example", "dft_walkthrough.html"],
+  // ["Digging Deeper", "dft_deeper.html"],
+  // ["USAGE and INTERPRETATION", "part5.html", "---"],
+  // ["Frequency Resolution", "dft_frequency.html"],
+  // ["Zero-Padding", "zeropadding.html"],
+  // ["Leakage", "dft_leakage.html"],
+  // ["Sine Wave Properties", "sine_wave_properties.html"],
+  // //["Convolution", "convolution.html"],
+  // ["Using FFT Libraries", "fft.html"],
+  // ["Conclusion", "conclusion.html"]
 ];
 
 
@@ -80,12 +80,12 @@ for (var i = 0; i < menuoptions.length; i++)
 
 
 var titleContents =
-'<div style="color: #ddd; font-size: 22px">SEEING <span style="color:rgb(93,217,241)">CIRCLES</span>, <span style="color:rgb(243,48,110);">SINES</span>, AND <span style="color:rgb(232,218,106);">SIGNALS</span><!--<svg id="phasortitle" class="svgWithText" width="500" height="20" style="display: inline">--></svg></div><div class="subheader" style="margin-top: 0px; color: #888; font-size: 18px; width: 800px">A COMPACT PRIMER ON DIGITAL SIGNAL PROCESSING<span id="icons" style="margin-left: 10"></span></div>';
+'<div style="color: #ddd; font-size: 22px"> <span style="color:rgb(93,217,241)">GUN</span> <span style="color:rgb(243,48,110);">LEGISLATION</span> AND <span style="color:rgb(232,218,106);">ACTIVISM</span><!--<svg id="phasortitle" class="svgWithText" width="500" height="20" style="display: inline">--></svg></div><div class="subheader" style="margin-top: 0px; color: #888; font-size: 18px; width: 800px">A RESOURCE TO UNDERSTAND GUN LAWS AND HOW TO CHANGE THEM<span id="icons" style="margin-left: 10"></span></div>';
 
 document.getElementById('titleinfo').innerHTML = titleContents;
 
-document.getElementById('icons').innerHTML = 
-'<span class="icon-twitter bigicon" style="color: #aaa; cursor: pointer;" onclick="window.open(\'http://www.twitter.com/jackschaedler\');"> <span style="font-size:10px; color: #555"></span></span><span class="icon-github-circled bigicon" style="color: #aaa; cursor: pointer" onclick="window.open(\'http://www.github.com/jackschaedler/circles-sines-signals\');"><span style="font-size:10px; padding-left: 4px; color: #555"></span></span>';
+// document.getElementById('icons').innerHTML = 
+// '<span class="icon-twitter bigicon" style="color: #aaa; cursor: pointer;" onclick="window.open(\'http://www.twitter.com/jackschaedler\');"> <span style="font-size:10px; color: #555"></span></span><span class="icon-github-circled bigicon" style="color: #aaa; cursor: pointer" onclick="window.open(\'http://www.github.com/jackschaedler/circles-sines-signals\');"><span style="font-size:10px; padding-left: 4px; color: #555"></span></span>';
 
 if (document.getElementById('footer') != null)
 {
